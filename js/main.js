054d57d98d3976e295ee2506eccc466578d0a7aa
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-06-13 14:56:18 -0400

    construct item

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var itemText;
+var itemBody;
+
+$(document).ready(function(){
+	$('#addButton').click(function(){
+		itemText = $(this).closest('#add').find('#addText').text();
+		constructItem(itemText);
+		
+
+	});
+});
+
+function constructItem(itemText){
+	itemBody = '<li class="itemCounter"></li><div class="itemBody"><button name="delete" class="delete">X</button><p class="itemText">' + itemText + '</p><input type="checkbox" name="itemCheck" value="itemCheck"></div>';
+}
