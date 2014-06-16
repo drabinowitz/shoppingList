d66acfa1168c2116c5613ff3b2bd509debe880fd
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-06-16 09:08:04 -0400

    refactored sortable

--- js/events.js
+++ js/events.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
-	$('#itemList').sortable();
-
 	$('#add').on('click','#addButton',addItem);
 
-	$('#itemList').on('click','.delete',deleteItem)
+	$('#itemList').sortable()
 
-	.on('click','.itemText',editItem);
+		.on('click','.delete',deleteItem)
+
+		.on('click','.itemText',editItem);
 
 });
